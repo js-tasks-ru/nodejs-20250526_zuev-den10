811e9b4e378071f6f4ec7a097129a714400d2e06
DenFKSR <zuev.den10@mail.ru>
2025-06-15 16:57:30 +0300

    add solution for 3-module 1-task

--- 03-di/01-notification-service/tasks/tasks.service.ts
+++ 03-di/01-notification-service/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto, Task, TaskStatus, UpdateTaskDto } from "./task.model";
+import { NotificationsService } from '../notifications/notifications.service';
+import {UsersService} from '../users/users.service';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private readonly notificationsService: NotificationsService, private readonly usersService: UsersService) {}
 
   async createTask(createTaskDto: CreateTaskDto) {
     const { title, description, assignedTo } = createTaskDto;
@@ -18,6 +20,11 @@ export class TasksService {
     };
     this.tasks.push(task);
 
+    const user =this.usersService.getUserById(Number(task.id))
+    const to = user.email
+    const message =`Вы назначены ответственным за задачу: \"${title}\"` 
+    const subject = `Новая задача`
+   const notifications = this.notificationsService.sendEmail(to,subject,message)
     return task;
   }
 
@@ -26,8 +33,12 @@ export class TasksService {
     if (!task) {
       throw new NotFoundException(`Задача с ID ${id} не найдена`);
     }
-
     Object.assign(task, updateTaskDto);
+    const user = this.usersService.getUserById(task.assignedTo);
+    const to = user.phone
+    const message = `Статус задачи "${task.title}" обновлён на "${task.status}"`;
+    const notifications = this.notificationsService.sendSMS(to,message)
+    
     return task;
   }
 }
