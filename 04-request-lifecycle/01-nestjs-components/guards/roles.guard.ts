5111c037078a130afa3ed28c60c0b16f8d7f7034
DenFKSR <zuev.den10@mail.ru>
2025-06-23 19:20:15 +0300

    add solution for 4-module 1-task

--- 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
+++ 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, ForbiddenException } from "@nestjs/common";
 
 export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {}
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const userRole = request.headers['x-role']; 
+    if (userRole !="admin"){
+       throw  new ForbiddenException("Доступ запрещён: требуется роль admin")
+    }
+    return true; 
+
+
+
+  }
 }
